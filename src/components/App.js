bcf97191516d7c4953b37870452aab2de1e257a1
CbnMarian <m.marian03@yahoo.com>
2023-12-13 15:39:24 +0200

    last

---
+++ src/components/App.js
@@ -0,0 +1,45 @@
+import Logo from "./lgo";
+import Form from "./form";
+import PackingList from "./PackingList";
+import { useState } from "react";
+import Stats from "./Stats";
+export default function App() {
+  const [items, setItems] = useState([]);
+
+  function handleAddItems(item) {
+    setItems((items) => [...items, item]);
+  }
+
+  function handleDeleteItem(id) {
+    setItems((items) => items.filter((item) => item.id !== id));
+  }
+
+  function handleToggleItem(id) {
+    setItems((items) =>
+      items.map((item) =>
+        item.id === id ? { ...item, packed: !item.packed } : item
+      )
+    );
+  }
+
+  function handleClearList() {
+    const confirmed = window.confirm(
+      "Are you Sure you want to delete all the items?"
+    );
+    if (confirmed) setItems([]);
+  }
+
+  return (
+    <div className="App">
+      <Logo />
+      <Form onAddItems={handleAddItems} />
+      <PackingList
+        items={items}
+        onDeleteItem={handleDeleteItem}
+        onToggleItems={handleToggleItem}
+        onClearList={handleClearList}
+      />
+      <Stats items={items} />
+    </div>
+  );
+}
